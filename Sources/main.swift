ede505564ac90bfb7833c44c48b15329ec7e12d7
iwa <iwaongit@gmail.com>
2025-06-26 17:27:59 +0100

    feat: add some guards for DOMAINS env var

--- Sources/main.swift
+++ Sources/main.swift
@@ -125,7 +125,19 @@ struct DomainExpiryWatcher {
 func main() async {
     let domains = ProcessInfo.processInfo.environment["DOMAINS"]?.split(separator: ",").map(String.init) ?? []
 
+    guard !domains.isEmpty else {
+        print("No domains provided. Set the DOMAINS environment variable as comma-separated values.")
+        return
+    }
+
     for domain in domains {
+        guard !domain.isEmpty else {
+            print("[WARN] Skipping empty domain entry.")
+            continue
+        }
+
+        let domain = domain.trimmingCharacters(in: .whitespacesAndNewlines)
+
         print("\n --- \(domain) ---")
         let expiryDate = try? await DomainExpiryWatcher.getExpiryDate(domain: domain)
         if let expiryDate = expiryDate {

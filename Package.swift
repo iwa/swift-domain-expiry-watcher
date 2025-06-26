70fae219e9ca1055c0d16e6125ecb323a001e711
iwa <iwaongit@gmail.com>
2025-06-26 17:13:51 +0100

    chore: bump minimum macos version to v13

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "domain-expiry-watcher",
+    platforms: [
+        // Specify the platforms supported by this package.
+        .macOS(.v13), // Minimum macOS version
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.

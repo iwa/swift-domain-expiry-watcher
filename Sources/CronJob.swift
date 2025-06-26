700b5134b73520f93f7209354d3a7106ba16c727
iwa <iwaongit@gmail.com>
2025-06-26 22:50:31 +0100

    chore: edit cronjob to be hourly

--- Sources/CronJob.swift
+++ Sources/CronJob.swift
@@ -2,7 +2,7 @@ import Vapor
 import VaporCron
 
 struct ComplexJob: VaporCronSchedulable {
-    static var expression: String { "* * * * *" }
+    static var expression: String { "* * * * 0" } // hourly
 
     static func task(on application: Application) -> EventLoopFuture<Void> {
         return application.eventLoopGroup.future().always { _ in

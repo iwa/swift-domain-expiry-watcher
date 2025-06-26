fe46ea990efe94fe1095177580915b2a26042d9f
iwa <iwaongit@gmail.com>
2025-06-26 21:42:44 +0100

    chore(deps): add VaporCron

--- Package.swift
+++ Package.swift
@@ -9,10 +9,16 @@ let package = Package(
         // Specify the platforms supported by this package.
         .macOS(.v13), // Minimum macOS version
     ],
+    dependencies: [
+        .package(url: "https://github.com/mihaelisaev/VaporCron.git", from: "2.6.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "domain-expiry-watcher"),
+            name: "domain-expiry-watcher",
+            dependencies: [
+                .product(name: "VaporCron", package: "VaporCron")
+            ])
     ]
 )

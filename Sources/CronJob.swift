8582456bd0a84b24733b81eac357c76e77bb7184
iwa <iwaongit@gmail.com>
2025-07-01 16:47:21 +0200

    feat: edit cron to be daily

--- Sources/CronJob.swift
+++ Sources/CronJob.swift
@@ -2,7 +2,7 @@ import Vapor
 import VaporCron
 
 struct UpdateDomainsJob: VaporCronSchedulable {
-    static var expression: String { "* * * * 0" } // hourly
+    static var expression: String { "* * * 0 0" } // daily at midnight
 
     static func task(on application: Application) -> EventLoopFuture<Void> {
         return application.eventLoopGroup.future().always { _ in
